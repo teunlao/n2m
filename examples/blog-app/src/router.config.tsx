2d424589410768d93b0b031a12b7239b88e7b8dc
Dmitriy Osipov <teunlao@gmail.com>
2024-07-31 18:23:28 +0200

    feat: update home page

--- examples/blog-app/src/router.config.tsx
+++ examples/blog-app/src/router.config.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { defineRouter } from '@n2m/router/react'
 import { createRoute } from '@n2m/router'
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Heading, Link, Text, VStack } from '@chakra-ui/react'
 import { SegmentContainerCached } from '@n2m/core-modules/react'
 import { ArticleSegmentToken, ArticlesSegmentToken } from './modules/articles/tokens.ts'
 import { LayoutSegmentToken } from './modules/layout/tokens.ts'
@@ -23,7 +23,30 @@ export const renderProps = defineRouter({
     Home: {
       layout: MainLayout,
       route: createRoute({ name: 'Home', meta: metaConfig.index }),
-      view: () => <Box>Home</Box>,
+      view: () => (
+        <Box>
+          <Box p={5}>
+            <VStack align="start" gap={5}>
+              <Heading as="h1" size="xl">
+                Blog app example
+              </Heading>
+              <Flex>
+                <Text mr={2}>App deployed at:</Text>
+                <Link href="https://n2m.onrender.com/">
+                  <Text color="gray.400">https://n2m.onrender.com</Text>
+                </Link>
+              </Flex>
+
+              <Flex>
+                <Text mr={2}>Payload CMS deployed at:</Text>
+                <Link href="https://n2m-payload.onrender.com/">
+                  <Text color="gray.400">https://n2m-payload.onrender.com</Text>
+                </Link>
+              </Flex>
+            </VStack>
+          </Box>
+        </Box>
+      ),
       path: '/',
     },
     Articles: {
