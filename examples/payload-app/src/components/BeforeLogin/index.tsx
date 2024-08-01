784ddbbbbc5f34da8df59e0e09fafbaf25ece04a
Dmitriy Osipov <teunlao@gmail.com>
2024-08-01 08:49:13 +0200

    fix: links

--- examples/payload-app/src/components/BeforeLogin/index.tsx
+++ examples/payload-app/src/components/BeforeLogin/index.tsx
@@ -4,12 +4,15 @@ const BeforeLogin: React.FC = () => {
   return (
     <div>
       <p>
-        To log in, use the email <strong>demo@payloadcms.com</strong> with the password <strong>demo</strong>. This demo
-        CMS will reset every hour.
+        To log in, use the email <strong>demo@payloadcms.com</strong> with the password <strong>demo</strong>.
       </p>
       <p>
         The code for this demo is open source and can be found{' '}
-        <a href="https://github.com/payloadcms/public-demo" rel="noopener noreferrer" target="_blank">
+        <a
+          href="https://github.com/teunlao/n2m/tree/main/examples/payload-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           here
         </a>
         .
